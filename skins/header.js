add0d79199b4ead2cdde15bfff6cbd4abd7ac9e5
Luca Matteis <lmatteis@gmail.com>
2011-06-08 09:36:22 +0200

    Added google analytics

--- skins/header.js
+++ skins/header.js
@@ -26,6 +26,19 @@ this.out += 'Il Grillo Mangiante</title> \
 <script src="/js/MooEditable.js"></script> \
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script> \
 <script type="text/javascript" src="/js/ui.js"></script> \
+<script type="text/javascript">\
+\
+  var _gaq = _gaq || [];\
+  _gaq.push(['_setAccount', 'UA-3942737-3']);\
+  _gaq.push(['_trackPageview']);\
+\
+  (function() {\
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\
+  })();\
+\
+</script>\
 </head> \
 <body id="home"> \
 	\

d28c841b31fdc91230bfc69f5e20939e9d799e17
Luca Matteis <lmatteis@gmail.com>
2011-06-08 09:45:57 +0200

    removed font and tried to follow yslow

--- skins/header.js
+++ skins/header.js
@@ -19,7 +19,6 @@ this.out += 'Il Grillo Mangiante</title> \
 <!-- just so dev is faster<link href=\'http://fonts.googleapis.com/css?family=Cabin:regular,bold\' rel="stylesheet" type="text/css">--> \
 <link type="text/css" rel="stylesheet" href="/stylesheets/reset.css" /> \
 <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" /> \
-<link href="http://fonts.googleapis.com/css?family=Copse" rel="stylesheet" type="text/css">\
 <link href="http://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">\
  \
 <script src="/js/mootools-yui-compressed.js"></script> \
@@ -29,13 +28,13 @@ this.out += 'Il Grillo Mangiante</title> \
 <script type="text/javascript">\
 \
   var _gaq = _gaq || [];\
-  _gaq.push(['_setAccount', 'UA-3942737-3']);\
-  _gaq.push(['_trackPageview']);\
+  _gaq.push(["_setAccount", "UA-3942737-3"]);\
+  _gaq.push(["_trackPageview"]);\
 \
   (function() {\
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\
+    var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;\
+    ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";\
+    var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);\
   })();\
 \
 </script>\
